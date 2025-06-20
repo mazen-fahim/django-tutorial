e4329fd6116d1334346cfb1f22e1cc48a9d8325e
mazen-fahim <mazenfahim.g@gmail.com>
2025-06-21 00:59:37 +0300

    finish part 4

--- polls/views.py
+++ polls/views.py
@@ -1,37 +1,74 @@
-from django.http import Http404, HttpResponse
+from django.db.models import F
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Choice, Question
+
+# def index(request):
+#     latest_questions_list = Question.objects.order_by("-pub_date")[:5]
+#     # template = loader.get_template("polls/index.html")
+#     # context = {"latest_questions_list": latest_questions_list}
+#     # return HttpResponse(template.render(context, request))
+#     context = {"latest_questions_list": latest_questions_list}
+#     return render(request, "polls/index.html", context)
+#
+#
+# def detail(request, question_id):
+#     # response = "Your are looking at question %s."
+#     # return HttpResponse(response % question_id)
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#     # else:
+#     #     return render(request, "polls/detail.html", {"question": question})
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {"question": question})
+#
+#
+# def result(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/result.html", {"question": question})
 
 
-def index(request):
-    latest_questions_list = Question.objects.order_by("-pub_date")[:5]
-    # template = loader.get_template("polls/index.html")
-    # context = {"latest_questions_list": latest_questions_list}
-    # return HttpResponse(template.render(context, request))
-    context = {"latest_questions_list": latest_questions_list}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def detail(request, question_id):
-    # response = "Your are looking at question %s."
-    # return HttpResponse(response % question_id)
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    # else:
-    #     return render(request, "polls/detail.html", {"question": question})
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
 
-def result(request, question_id):
-    response = "Your are looking at the result for question %s."
-    return HttpResponse(response % question_id)
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "polls/result.html"
 
 
 def vote(request, question_id):
-    response = "Your are voting on question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            context={
+                "question": question,
+                "error_message": "You didn't select a choice!",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:result", args=(question.id,)))
+    # response = "Your are voting on question %s."
+    # return HttpResponse(response % question_id)
